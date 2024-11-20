7bcb16fbe4d4e0a457e708975ce4d0f04d937581
chaya7282 <52631833+chaya7282@users.noreply.github.com>
2024-11-20 09:58:42 +0200

    First version of project2
    First version of project2

---
+++ exe_2_QA_2024/src/main/java/exe_2_QA_2024/exe_2_QA_2024/App.java
@@ -0,0 +1,10 @@
+package exe_2_QA_2024.exe_2_QA_2024;
+
+/**
+ * Hello world fro chaya 2!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
